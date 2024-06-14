40ccf6691e1abfd4f3ed5734b7e9bcf4363d981e
Amit Sahu <amitsahu174507@gmail.com>
2024-06-14 23:53:08 +0530

    Function half done

--- script.js
+++ script.js
@@ -14,20 +14,53 @@ let numGuess = 1;
 
 let playGame = true;
 
-if (playGame) {
-  
+if(playGame) {
+    submit.addEventListener('click',function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess);
+    })
 }
 
+
+
 function validateGuess(guess) {
- 
+  if(isNaN(guess)){
+    alert('Please Enter avalid Number')
+  }
+  else if( guess < 1 || guess >100){
+    alert('Please Enter a valid number between 1 and 100')
+  }
+  else{
+    prevGuess.push(guess);
+    if(numGuess == 11){
+        displayGuess(guess)
+        displayMessage(`Game Over !!  Random Number was ${randomNumber}`)
+        endGame();
+    }
+    else{
+        displayGuess(guess)
+        checkGuess(guess)
+    }
+  }
 }
 
 function checkGuess(guess) {
-  
+  if(guess === randomNumber){
+    displayMessage(`Congratulations!! You Guessed it in ${numGuess} guesses`)
+    endGame();
+  }
+  else if(guess > randomNumber){
+    displayMessage('Too High');
+  }
+  else if(guess < randomNumber){
+    displayMessage('Too Low');
+  }
 }
 
 function displayGuess(guess) {
- 
+  userInput.value = ''
 }
 
 function displayMessage(message) {
